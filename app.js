1a3dbcca38a96c7d11c701e96be2014d44e6b099
Vizzyy <barnabas.vizy@gmail.com>
2020-08-16 16:32:18 -0400

    Working auth

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 const express = require('express');
-const config = require('./config/config')
-const app = express();
+const passport = require('passport');
 const fs = require('fs');
 const path = require('path');
+const Auth0Strategy = require('passport-auth0');
+const userInViews = require('./helpers/userInViews');
+const secured = require('./helpers/secured');
+const session = require('express-session');
+const config = require('./config/config')
+const secrets = require('./config/secrets')
+const home = require('./routes/home')
+const logs = require('./routes/logs')
+const door = require('./routes/door')
+const users = require('./routes/users')
+const streams = require('./routes/streams')
+const lights = require('./routes/lights')
+const auth = require('./routes/auth');
+
+// Configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+    {
+        domain: secrets.AUTH0_DOMAIN,
+        clientID: secrets.AUTH0_CLIENT_ID,
+        clientSecret: secrets.AUTH0_CLIENT_SECRET,
+        callbackURL: secrets.AUTH0_CALLBACK_URL
+    },
+    function (accessToken, refreshToken, extraParams, profile, done) {
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
+        // extraParams.id_token has the JSON Web Token
+        // profile has all the information from the user
+        return done(null, profile);
+    }
+);
+
+passport.use(strategy);
+
+// You can use this section to keep a smaller payload
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+const app = express();
 const env = config.envOptions[config.environment];
 const server = require('https').Server({
     ca: env.sslOptions.ca ? fs.readFileSync(env.sslPath + env.sslOptions.ca) : [],
@@ -12,19 +53,24 @@ const server = require('https').Server({
     rejectUnauthorized: env.sslOptions.rejectUnauthorized
 }, app);
 const io = require('socket.io')(server);
-const home = require('./routes/home')
-const logs = require('./routes/logs')
-const door = require('./routes/door')
-const users = require('./routes/users')
-const streams = require('./routes/streams')
-const lights = require('./routes/lights')
 
+app.use(session({
+    secret: secrets.sessionSecret,
+    cookie: {secure: true},
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routers
+app.use('/', auth);
 app.use('/favicon.ico', express.static('./public/favicon.ico'));
+app.use(secured()); // everything after this is secured
+app.use(userInViews());
 app.use('/lights', lights);
 app.use('/streams', streams);
 app.use('/users', users);
@@ -32,8 +78,47 @@ app.use('/door', door);
 app.use('/logs', logs);
 app.use('/', home);
 
-io.on('connect', socket => {
-    console.log("New connection.");
+io.on('connection', function (socket) {
+    socket.on('page_load', function (data) {
+        console.log(data+ " page loaded.");
+    });
+});
+
+// Handle auth failure error messages
+app.use(function (req, res, next) {
+    if (req && req.query && req.query.error) {
+        req.flash('error', req.query.error);
+    }
+    if (req && req.query && req.query.error_description) {
+        req.flash('error_description', req.query.error_description);
+    }
+    next();
+});
+
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (config.environment === 'dev') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// Production error handler
+// No stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 server.listen(config.PORT);
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,14 @@ router.get('/', function(req, res) {
     });
 });
 
+/* GET user profile. */
+router.get('/user', function (req, res, next) {
+    const { _raw, _json, ...userProfile } = req.user;
+    res.render('user', {
+        userProfile: JSON.stringify(userProfile, null, 2),
+        title: 'Profile page'
+    });
+});
+
 module.exports = router;
+
