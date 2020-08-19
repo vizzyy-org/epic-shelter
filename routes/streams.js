ab8464dfa995fe9c059ee433b7ef84d2ed7ceca4
Vizzyy <barnabas.vizy@gmail.com>
2020-08-19 14:16:31 -0400

    Implement streaming via 2way SSL

--- routes/streams.js
+++ routes/streams.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const {MjpegProxy} = require("../helpers/mjpeg-proxy");
+const secrets = require('../config/secrets')
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    res.render('streams', {
-        test : req.ip
-    });
+router.get('/', function(req, res){
+   res.render('streams');
 });
+
+router.get('/door', function(req, res, next) {
+    next();
+}, new MjpegProxy('https://'+secrets.HUB_HOST+'/video').proxyRequest);
 
 module.exports = router;
