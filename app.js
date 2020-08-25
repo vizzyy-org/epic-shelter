7e67e058802fbb5e24aee65865b52a7ca13385b8
Vizzyy <barnabas.vizy@gmail.com>
2020-08-25 00:50:00 -0400

    Harden SSL rating

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const fs = require('fs');
 const path = require('path');
 const passport = require('./config/passport');
@@ -24,7 +25,26 @@ const server = require('https').Server({
     key: fs.readFileSync(env.sslPath + env.sslOptions.key),
     cert: fs.readFileSync(env.sslPath + env.sslOptions.cert),
     requestCert: env.sslOptions.requestCert,
-    rejectUnauthorized: env.sslOptions.rejectUnauthorized
+    rejectUnauthorized: env.sslOptions.rejectUnauthorized,
+    ciphers: [
+        "ECDHE-RSA-AES256-SHA384",
+        "DHE-RSA-AES256-SHA384",
+        "ECDHE-RSA-AES256-SHA256",
+        "DHE-RSA-AES256-SHA256",
+        "ECDHE-RSA-AES128-SHA256",
+        "DHE-RSA-AES128-SHA256",
+        "HIGH",
+        "!aNULL",
+        "!eNULL",
+        "!EXPORT",
+        "!DES",
+        "!RC4",
+        "!MD5",
+        "!PSK",
+        "!SRP",
+        "!CAMELLIA"
+    ].join(':'),
+    honorCipherOrder: true
 }, app);
 const io = require('socket.io')(server);
 
@@ -33,6 +53,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet.hsts({
+    maxAge: 31536000000, //one year
+    includeSubDomains: true,
+    force: true
+}));
 app.use(session({
     secret: secrets.sessionSecret,
     cookie: {secure: true},
