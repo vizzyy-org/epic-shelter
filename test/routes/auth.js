6a6fea1abae8a0ac2a9fa75238df9a6ae86cc0dd
Vizzyy <barnabas.vizy@gmail.com>
2020-10-21 17:48:22 -0400

    test pipeline failure

--- test/routes/auth.js
+++ test/routes/auth.js
@@ -57,7 +57,7 @@ describe('Auth', () => {
                 .end((err, res) => {
                     if(err) { console.log(err); }
                     // console.log(res.text);
-                    res.should.have.status(403);
+                    res.should.have.status(400);
                     done();
                 });
         });
