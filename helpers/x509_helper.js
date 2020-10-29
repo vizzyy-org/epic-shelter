e4b291b70a85711cf165c9ec33f24c8c753b1f4a
Vizzyy <barnabas.vizy@gmail.com>
2020-10-28 23:39:38 -0400

    Tweak logging

--- helpers/x509_helper.js
+++ helpers/x509_helper.js
@@ -1,4 +1,3 @@
-const logging = require("./logging_helper");
 const secrets = require('/etc/pki/vizzyy/secrets');
 
 module.exports = function () {
@@ -6,10 +5,6 @@ module.exports = function () {
         if(secrets.environment === "prod") {
             console.log(secrets.environment)
             let incoming = req.connection.getPeerCertificate().subject.CN;
-            logging.append_to_log("Incoming request - CN: " + incoming + " - DEST: " + req.originalUrl);
-
-            // req.isAdmin = constants.admins.includes(incoming);
-            // req.isOwner = constants.owner.includes(incoming);
             req.user = {displayName: incoming};
             console.log(req.user);
         }
