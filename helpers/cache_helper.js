a22e1c6f9b9f373c84d5e9dba537cdd71ee11003
Vizzyy <barnabas.vizy@gmail.com>
2020-11-18 16:32:10 -0500

    Disable caching for POST + necessary routing changes

--- helpers/cache_helper.js
+++ helpers/cache_helper.js
@@ -5,17 +5,23 @@ let isDev = secrets.environment === "dev"
 
 module.exports = function (duration) {
     return (req, res, next) => {
-        let key =  '__express__' + req.originalUrl || req.url;
-        if(isDev) console.log("checking cache for: " + key);
+        if (req.method !== "GET") { // do not cache POST/PUT/DELETE/etc
+            if (isDev) console.log("Skipping cache for request method: " + req.method);
+            next();
+            return
+        }
+
+        let key = '__express__' + req.originalUrl || req.url;
+        if (isDev) console.log("checking cache for: " + key);
         let cacheContent = memCache.get(key);
-        if(cacheContent && req.method === "GET"){ // do not cache POST/PUT/DELETE/etc
-            if(isDev) console.log("Cache hit!");
-            res.send( cacheContent );
-        }else{
-            if(isDev) console.log("Cache miss!");
+        if (cacheContent) {
+            if (isDev) console.log("Cache hit!");
+            res.send(cacheContent);
+        } else {
+            if (isDev) console.log("Cache miss!");
             res.sendResponse = res.send;
             res.send = (body) => {
-                memCache.put(key,body,duration*1000);
+                memCache.put(key, body, duration * 1000);
                 res.sendResponse(body);
             }
             next();
