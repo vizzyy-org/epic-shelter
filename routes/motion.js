67334d6aaf5545e8e64df884cb4e6ebdb4bb6d88
Vizzyy <barnabas.vizy@gmail.com>
2020-12-31 21:56:23 -0500

    split up timestamp into more easily readable date + time

--- routes/motion.js
+++ routes/motion.js
@@ -13,9 +13,17 @@ router.get('/', function(req, res) {
 
 router.get('/data/:imageId', function(req, res) {
     db_helper.sendMotionAssetById(req.params.imageId).then(result => {
+        // incoming Time string will be something like:
+        // 2020-12-31-21:10:11-event1
+        let timestamp = result.Time.split("-event")[0]
+        let date = timestamp.substring(0, 10)
+        let time = timestamp.substring(11)
+
         res.send({
             'buffer': Buffer.from(result.Image).toString('base64'),
-            'timestamp': result.Time.split("-event")[0]
+            'time': time,
+            'date': date,
+            'timestamp': timestamp
         });
     }).catch(function(err) {
         console.error(err);
